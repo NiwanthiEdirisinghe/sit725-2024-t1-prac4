3c817e57e8732f88cae14233021353ab4a9121ae
Niwanthi Edirisinghe <niwanthiedirisinghe.95@gmail.com>
2024-04-09 18:05:40 +1000

    Initial commit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,52 +1,66 @@
+
 const clickMe = () => {
-    alert("Thanks for clicking me. Hope you have a nice day!");
+  alert("Thanks for clicking me. Hope you have a nice day!");
+};
+const submitForm = () => {
+  let formData = {
+    title: $("#title").val(),
+    image: $("#path").val(),
+    link: $("#color").val(),
+    description: $("#des").val(),
   };
-  
-  const cardList = [
-    {
-      title: "Kitten 2",
-      image: "images/kitten-2.png",
-      link: "About Kitten 2",
-      description: "Demo description about kitten 2",
-    },
-    {
-      title: "Kitten 3",
-      image: "images/kitten-3.jpeg",
-      link: "About Kitten 3",
-      description: "Demo description about kitten 3",
-    },
-  ];
-  
-  const addCards = (items) => {
-    items.forEach((item) => {
-      let itemToAppend =
-        '<div class="col s4 center-align">' +
-        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' +
-        item.image +
-        '">' +
-        '</div><div class="card-content">' +
-        '<span class="card-title activator grey-text text-darken-4">' +
-        item.title +
-        '<i class="material-icons right">more_vert</i></span><p><a href="#">' +
-        item.link +
-        "</a></p></div>" +
-        '<div class="card-reveal">' +
-        '<span class="card-title grey-text text-darken-4">' +
-        item.title +
-        '<i class="material-icons right">close</i></span>' +
-        '<p class="card-text">' +
-        item.description +
-        "</p>" +
-        "</div></div></div>";
-      $("#card-section").append(itemToAppend);
-    });
-  };
-  
-  $(document).ready(function () {
-    $(".materialboxed").materialbox();
-    $("#clickMeButton").click(() => {
-      clickMe();
-    });
-    addCards(cardList);
+
+  // Send a POST request to add the new project
+  $.post("/api/cards", formData, (response) => {
+    if (response.statusCode === 201) {
+      alert("New card was added successfully!");
+      location.reload();
+    } else {
+      alert("Failed to add new card. Please try again later.");
+    }
   });
-  
+};
+
+const addCards = (items) => {
+  items.forEach((item) => {
+    let itemToAppend =
+      '<div class="col s4 center-align">' +
+      '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' +
+      item.image +
+      '">' +
+      '</div><div class="card-content">' +
+      '<span class="card-title activator grey-text text-darken-4">' +
+      item.title +
+      '<i class="material-icons right">more_vert</i></span><p><a href="#">' +
+      item.link +
+      "</a></p></div>" +
+      '<div class="card-reveal">' +
+      '<span class="card-title grey-text text-darken-4">' +
+      item.title +
+      '<i class="material-icons right">close</i></span>' +
+      '<p class="card-text">' +
+      item.description +
+      "</p>" +
+      "</div></div></div>";
+    $("#card-section").append(itemToAppend);
+  });
+};
+
+const getProjects = () => {
+  $.get("/api/cards", (response) => {
+    if (response.statusCode == 200) {
+      addCards(response.data);
+    }
+  });
+};
+
+$(document).ready(function () {
+  $(".materialboxed").materialbox();
+  $("#formSubmit").click(() => {
+    submitForm();
+  });
+
+  getProjects();
+
+  $(".modal").modal();
+});
--- server.js
+++ server.js
@@ -1,12 +1,64 @@
-var express = require("express")
-var app = express()
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.use(express.static(__dirname+'/public'))
+const app = express();
+const port = 3000;
+
+const password = encodeURIComponent("Ru4190416niwa#0");
+const uri = `mongodb+srv://niwanthiedirisinghe95:${password}@cluster1.w8lef3h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
+// Define the schema
+const catSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    link: String,
+    description: String,
+});
+
+// Create the Cat model
+const Cat = mongoose.model("Cat", catSchema);
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connect();
+
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-var port = process.env.port || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(port,()=>{
-    console.log("App listening to: "+port)
-})
+app.get("/api/cards", async (req, res) => {
+  try {
+    const cardList = await Cat.find();
+    res.json({ statusCode: 200, data: cardList, message: "Success" });
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, message: "Internal Server Error" });
+  }
+});
+
+app.post("/api/cards", async (req, res) => {
+  try {
+    const { title, image, link, description } = req.body;
+    
+    const card = new Cat({ title, image, link, description });
+    await card.save();
+
+    res.status(201).json({ statusCode: 201, message: "New cat was added successfully!", data: card });
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, message: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log("App listening to: " + port);
+});
